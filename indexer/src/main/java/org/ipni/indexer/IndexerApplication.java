65d9a4df71e8e245f2cda330bc619238e353a259
John Iacona <J.Iacona@kew.org>
2017-06-07 11:59:40 +0100

    Initial project with indexer and web submodules

---
+++ indexer/src/main/java/org/ipni/indexer/IndexerApplication.java
@@ -0,0 +1,12 @@
+package org.ipni.indexer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndexerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndexerApplication.class, args);
+	}
+}
---
+++ web/src/main/java/org/ipni/IpniApplication.java
@@ -0,0 +1,12 @@
+package org.kew.ipni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpniApplication.class, args);
+	}
+}
