7b6fd9752f8efae758eecfa71f0a2b143e2725a5
James Crowe <james.crowe@kew.org>
2017-06-16 16:52:58 +0100

    added suggester

--- web/src/main/java/org/ipni/controller/SearchController.java
+++ web/src/main/java/org/ipni/controller/SearchController.java
@@ -1,14 +1,18 @@
 package org.ipni.controller;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
 import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.client.solrj.response.SuggesterResponse;
 import org.ipni.response.Response;
+import org.ipni.search.AutoCompleteBuilder;
 import org.ipni.search.QueryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,15 +22,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.google.common.collect.ImmutableList;
+
 @Controller
 @RequestMapping("/api/1/")
 public class SearchController {
 
 	private SolrClient solrClient = null;
 	
+	private static List<String> suggesters = ImmutableList.<String>of(
+			"scientific-name");
+	
 	@Autowired
 	public void setSolrClient() {
-		this.solrClient = new HttpSolrClient("http://solr:8983/solr/ipni_view");
+		this.solrClient = new HttpSolrClient.Builder("http://solr:8983/solr/ipni_view").build();
 	}
 	
 	@GetMapping("search")
@@ -40,5 +49,26 @@ public class SearchController {
 		Response response = new Response(queryResponse);
 		return new ResponseEntity<Response>(response, HttpStatus.OK);
 		
+	}
+	
+	@GetMapping("suggest")
+	public ResponseEntity<SuggesterResponse> suggest(
+			@RequestParam(value = "query", required = true) String queryString,
+			@RequestParam(value = "page.size", required = false, defaultValue = "5") Integer pageSize
+			) throws SolrServerException, IOException {
+
+		SolrQuery query = new AutoCompleteBuilder()
+				.setWorkingSuggesters(suggesters, solrClient)
+				.setSuggesters(suggesters)
+				.pageSize(pageSize)
+				.setQuery(queryString)
+				.build();
+
+		if(query != null) {
+			SuggesterResponse response = solrClient.query(query).getSuggesterResponse();
+			return new ResponseEntity<SuggesterResponse>(response, HttpStatus.OK);
+		}
+
+		return null;
 	}
 }
---
+++ web/src/main/java/org/ipni/search/AutoCompleteBuilder.java
@@ -0,0 +1,84 @@
+package org.ipni.search;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.SolrResponseBase;
+import org.apache.solr.common.params.ModifiableSolrParams;
+import org.apache.solr.common.util.NamedList;
+
+import com.google.common.collect.ImmutableSet;
+
+public class AutoCompleteBuilder {
+
+	private SolrQuery query = new SolrQuery().setRequestHandler("/suggest");
+
+	private Integer pageSize = 5;
+
+	private List<String> workingSuggesters;
+	
+	private boolean suggesterSet = false;
+
+	private void addWorkingSuggester(String suggester) {
+		if(workingSuggesters != null && workingSuggesters.contains(suggester)) {
+			query.add("suggest.dictionary", suggester);
+			suggesterSet = true;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public AutoCompleteBuilder setWorkingSuggesters(List<String> suggesters, SolrClient client) throws SolrServerException, IOException {
+		workingSuggesters = new ArrayList<String>();
+		ModifiableSolrParams params = new ModifiableSolrParams()
+				.add("key", "suggest")
+				.add("stats", "true");
+		SolrQuery query = new SolrQuery().setRequestHandler("/admin/mbeans");
+		query.add(params);
+		SolrResponseBase response = client.query(query);
+		NamedList<Object> responseStats = (NamedList<Object>) response.getResponse().findRecursive("solr-mbeans","OTHER","suggest","stats");
+		for(Entry<String, Object> suggester : responseStats){
+			if(!suggester.getKey().equals("totalSizeInBytes") 
+					&& !(((String) suggester.getValue()).contains("sizeInBytes=0"))) {
+				workingSuggesters.add(suggester.getKey());
+			}
+		}
+		return this;
+	}
+
+	public AutoCompleteBuilder addSuggester(String suggester) {
+			addWorkingSuggester(suggester);
+		return this;
+	}
+
+	public AutoCompleteBuilder setSuggesters(List<String> suggesters) {
+		for(String suggester : suggesters) {
+			addSuggester(suggester);
+		}
+		return this;
+	}
+
+	public AutoCompleteBuilder setQuery(String string) {
+		query.setQuery(string);
+		return this;
+	}
+
+	public AutoCompleteBuilder pageSize(Integer pageSize) {
+		this.pageSize = pageSize;
+		return this;
+	}
+
+	public SolrQuery build () {
+		if (suggesterSet) {
+			query.add("suggest.count", pageSize.toString());
+			return query;
+		}
+		return null;
+	}
+}
+
