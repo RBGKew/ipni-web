1a09a81f74183ec676025a5b402cb1ada93f3386
John Iacona <J.Iacona@kew.org>
2017-06-26 17:23:34 +0100

    Map search fields

--- web/src/main/java/org/ipni/constants/FieldMapping.java
+++ web/src/main/java/org/ipni/constants/FieldMapping.java
@@ -2,31 +2,60 @@ package org.ipni.constants;
 
 public enum FieldMapping {
 
-	scientificName("scientific_name", "taxon_scientific_name_s_lower"),
+	scientificName("scientific name", "taxon_scientific_name_s_lower"),
 	family("family", "family_s_lower"),
 	genus("genus", "genus_s_lower"),
 	species("species", "species_s_lower"),
 	infraspecies("infraspecies", "infraspecies_s_lower"),
 	rank("rank", "taxon_rank_s_lower"),
 	hybrid("hybrid", "hybrid_s_lower"),
-	hybridGenus("hybrid_genus", "hybrid_genus_s_lower"),
+	hybridGenus("hybrid genus", "hybrid_genus_s_lower"),
 	author("author", "authors_s_lower"),
 	reference("reference", "reference_s_lower");
 
 	private String solrField;
 	private String apiField;
-	
-	private FieldMapping(final String apiField, final String solrField){
+
+	private FieldMapping(final String apiField, final String solrField) {
 		this.solrField = solrField;
 		this.apiField = apiField;
 	}
-	
-	public String solrField(){
+
+	public String solrField() {
 		return solrField;
 	}
-	
-	public String apiField(){
+
+	public String apiField() {
 		return apiField;
 	}
-	
+
+	public static boolean isValidField(String field) {
+		for(FieldMapping f : values()) {
+			if(f.apiField.equals(field)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public static String getSolrFieldFrom(String apiField) {
+		for(FieldMapping f : values()) {
+			if(f.apiField.equals(apiField)) {
+				return f.solrField;
+			}
+		}
+
+		return null;
+	}
+
+	public static String getApiFieldFrom(String solrField) {
+		for(FieldMapping f : values()) {
+			if(f.solrField.equals(solrField)) {
+				return f.apiField;
+			}
+		}
+
+		return null;
+	}
 }
--- web/src/main/java/org/ipni/search/SingleFieldFilterQuery.java
+++ web/src/main/java/org/ipni/search/SingleFieldFilterQuery.java
@@ -1,6 +1,7 @@
 package org.ipni.search;
 
 import org.apache.solr.client.solrj.SolrQuery;
+import org.ipni.constants.FieldMapping;
 
 public class SingleFieldFilterQuery extends QueryOption {
 
@@ -8,10 +9,14 @@ public class SingleFieldFilterQuery extends QueryOption {
 	public void addQueryOption(String key, String value, SolrQuery query) {
 		value = prepareValue(key, value);
 		
-		if(query.getQuery() == null) {
-			query.setQuery(String.format("%s:%s", key, value));
-		} else {
-			query.setQuery(String.format("%s AND %s:%s", query.getQuery(), key, value));
+		if(FieldMapping.isValidField(key)) {
+			key = FieldMapping.getSolrFieldFrom(key);
+
+			if(query.getQuery() == null) {
+				query.setQuery(String.format("%s:%s", key, value));
+			} else {
+				query.setQuery(String.format("%s AND %s:%s", query.getQuery(), key, value));
+			}
 		}
 	}
 }
