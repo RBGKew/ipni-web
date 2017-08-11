b0468c88e3aa3418cbbbabff1e0ecd9c2381c6a0
John Iacona <J.Iacona@kew.org>
2017-08-11 15:25:43 +0100

    Add "fullName" helper in java helpers

--- web/src/main/java/org/ipni/view/NameHelper.java
+++ web/src/main/java/org/ipni/view/NameHelper.java
@@ -2,15 +2,34 @@ package org.ipni.view;
 
 import org.ipni.model.Name;
 
+import com.github.jknack.handlebars.Handlebars;
+
 import pl.allegro.tech.boot.autoconfigure.handlebars.HandlebarsHelper;
 
 @HandlebarsHelper
 public class NameHelper {
-	public CharSequence showName(Name taxon) {
-		if(taxon.getName() != null) {
-			return taxon.getName();
+	public CharSequence showName(Name citation) {
+		if(citation.getName() != null) {
+			return citation.getName();
 		} else {
-			return taxon.getFamily();
+			return citation.getFamily();
 		}
+	}
+
+	public CharSequence fullName(Name citation) {
+		StringBuffer name = new StringBuffer();
+		name.append("<i>");
+		name.append(showName(citation));
+		name.append("</i> ");
+
+		if(citation.getAuthors() != null) {
+			name.append(citation.getAuthors());
+		}
+
+		if(citation.getReference() != null) {
+			name.append(citation.getReference());
+		}
+
+		return new Handlebars.SafeString(name);
 	}
 }
