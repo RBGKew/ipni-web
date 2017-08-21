8352eeba167f1198b5da8959b998c24fa9ef5f77
John Iacona <J.Iacona@kew.org>
2017-08-21 14:56:39 +0100

    Tweak query builder to allow for * searches

--- web/src/main/java/org/ipni/search/MultiFieldQuery.java
+++ web/src/main/java/org/ipni/search/MultiFieldQuery.java
@@ -1,35 +1,39 @@
 package org.ipni.search;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import org.apache.solr.client.solrj.SolrQuery;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.google.common.base.Joiner;
 
 public class MultiFieldQuery extends QueryOption {
 
-	private static final Logger logger = LoggerFactory.getLogger(MultiFieldQuery.class);
-	private ArrayList<String> searchFields;
+	Set<String> fields;
 
 	public MultiFieldQuery(Set<String> fields) {
-		this.searchFields = new ArrayList<String>(fields.size());
-		for(String field : fields) {
-			searchFields.add(String.format("%s:%s", field, prepareValue(field, "%1$s")));
-		}
+		this.fields = fields;
 	}
 
 	@Override
 	public void addQueryOption(String key, String value, SolrQuery query) {
-		if(value != null && !value.isEmpty()){
-			String newQuery = String.format(Joiner.on(" OR ").join(searchFields), value);
+		if(value != null && !value.isEmpty()) {
+			String newQuery = Joiner.on(" OR ").join(prepareValues(value));
 			if(query.getQuery() == null) {
 				query.setQuery("(" + newQuery + ")");
 			} else {
 				query.setQuery(query.getQuery() + " AND (" + newQuery + ")");
 			}
 		}
+	}
+
+	private List<String> prepareValues(String value) {
+		ArrayList<String> values = new ArrayList<>(fields.size());
+
+		for(String field : fields) {
+			values.add(String.format("%s:%s", field, prepareValue(field, value)));
+		}
+
+		return values;
 	}
 }
--- web/src/main/java/org/ipni/search/QueryOption.java
+++ web/src/main/java/org/ipni/search/QueryOption.java
@@ -5,18 +5,13 @@ import org.apache.solr.client.solrj.SolrQuery;
 public abstract class QueryOption {
 	
 	protected String prepareValue(String field, String value) {
-		if(field.endsWith("_s") || field.endsWith("_s_lower") || field.endsWith("_ss_lower")) {
-			// string fields should be quoted to exactly match multi-word values
+		// string fields with spaces should be quoted to exactly match multi-word values
+		if(value.contains(" ") || value.contains("+")) {
 			return String.format("\"%s\"", value);
-		} else if (field.endsWith("_t")){
-			// general text fields are quoted with a phrase slop 
-			return String.format("\"%s\"~10", value);
 		} else {
-			// otherwise, encode spaces
-			return value.replace(" ", "+");
+			return value;
 		}
 	}
-	
-	public abstract void addQueryOption(String key, String value, SolrQuery query);
 
+	public abstract void addQueryOption(String key, String value, SolrQuery query);
 }
