f88c02dd298dfb748e0c9d59a664fd1f117d6d3d
John Iacona <j.iacona@kew.org>
2017-10-27 13:58:46 +0100

    Sort by ranks, not full name string

--- web/src/main/java/org/ipni/search/SortQuery.java
+++ web/src/main/java/org/ipni/search/SortQuery.java
@@ -1,8 +1,9 @@
 package org.ipni.search;
 
+import static org.ipni.constants.FieldMapping.*;
+
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrQuery.ORDER;
-import org.ipni.constants.FieldMapping;
 
 public class SortQuery extends QueryOption {
 
@@ -10,16 +11,26 @@ public class SortQuery extends QueryOption {
 	public void addQueryOption(String key, String value, SolrQuery query) {
 		switch(value){
 		case "name_asc":
-			query.setSort("sortable", ORDER.asc);
+			query.setSort(family.solrField(), ORDER.asc)
+					.addSort(infrafamily.solrField(), ORDER.asc)
+					.addSort(genus.solrField(), ORDER.asc)
+					.addSort(infragenus.solrField(), ORDER.asc)
+					.addSort(species.solrField(), ORDER.asc)
+					.addSort(infraspecies.solrField(), ORDER.asc);
 			break;
 		case "name_desc":
-			query.setSort("sortable", ORDER.desc);
+			query.setSort(family.solrField(), ORDER.desc)
+					.addSort(infrafamily.solrField(), ORDER.desc)
+					.addSort(genus.solrField(), ORDER.desc)
+					.addSort(infragenus.solrField(), ORDER.desc)
+					.addSort(species.solrField(), ORDER.desc)
+					.addSort(infraspecies.solrField(), ORDER.desc);
 			break;
 		case "published_asc":
-			query.setSort(FieldMapping.yearPublished.solrField(), ORDER.asc);
+			query.setSort(yearPublished.solrField(), ORDER.asc);
 			break;
 		case "published_desc":
-			query.setSort(FieldMapping.yearPublished.solrField(), ORDER.desc);
+			query.setSort(yearPublished.solrField(), ORDER.desc);
 			break;
 		}
 	}
